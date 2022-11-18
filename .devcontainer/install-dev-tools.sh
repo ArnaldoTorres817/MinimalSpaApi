1d26d5e98d177ba6a1a646b6705b0828eb044273
Arnaldo Torres <torres.arnaldo817@gmail.com>
2022-11-18 05:26:41 +0000

    chore: :construction_worker: add nvm installation to shell script

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -3,4 +3,11 @@
 sudo apt update
 sudo apt upgrade -y curl git zsh
 
+# install nvm (node version manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
+
+# oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
